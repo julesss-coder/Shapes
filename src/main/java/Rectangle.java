e6e55bedd6995edb5d15acf876fa397465ae2fad
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-02-10 20:19:30 +0100

    Create Rectangle.java

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,23 @@
+public class Rectangle extends Shape {
+    double width;
+    double length;
+
+    public Rectangle(String color, Boolean isFilled, double width, double length) {
+        super(color, isFilled);
+        if (width <= 0 || length <= 0) {
+            throw new IllegalArgumentException("Length and width must be > 0.");
+        }
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
